778bf82530104286b0c25ca28c0125217c73d72c
Hack Reactor Students <github@hackreactor.com>
2013-05-04 17:14:33 -0700

    Move dancers more interesting.

--- src/moveyDancer.js
+++ src/moveyDancer.js
@@ -6,5 +6,6 @@ MoveyDancer.prototype = Object.create(Dancer.prototype);
 MoveyDancer.prototype.constructor = MoveyDancer;
 
 MoveyDancer.prototype.action = function(){
-  this.$node.css('top', '+=10');
+  this.$node.css('top', '+=' + (Math.random() * 100 - 50));
+  this.$node.css('left', '+=' + (Math.random() * 100 - 50));
 };
